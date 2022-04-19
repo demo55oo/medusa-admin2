d8d57e969ca06b9d7c1dbf1281fc6f1c1d2549c9
fPolic <frane@medusajs.com>
2022-04-19 13:20:35 +0200

    wip: setup modal

--- src/domain/pricing/index.tsx
+++ src/domain/pricing/index.tsx
@@ -1,16 +1,19 @@
 import { Router, RouteComponentProps } from "@reach/router"
 import { navigate } from "gatsby"
-import React from "react"
+import React, { useState } from "react"
 import PlusIcon from "../../components/fundamentals/icons/plus-icon"
 import BodyCard from "../../components/organisms/body-card"
 import TableViewHeader from "../../components/organisms/custom-table-header"
 import PricingTable from "./pricing-table"
+import BulkEditorContainer from "./bulk-editor"
 
 const PricingIndex: React.FC<RouteComponentProps> = () => {
+  const [show, setShow] = useState(false) // TODO: remove
+
   const actionables = [
     {
       label: "Add price list",
-      onClick: () => navigate(`/a/pricing/new`),
+      onClick: () => setShow(true),
       icon: <PlusIcon size={20} />,
     },
   ]
@@ -25,6 +28,7 @@ const PricingIndex: React.FC<RouteComponentProps> = () => {
           <PricingTable />
         </BodyCard>
       </div>
+      {show && <BulkEditorContainer />}
     </div>
   )
 }
