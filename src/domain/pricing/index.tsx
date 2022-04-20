2286717441aefaa262ad3bbe9e4276ef1b2ec429
fPolic <frane@medusajs.com>
2022-04-20 17:35:54 +0200

    wip: header tiles and components, rendering product sections

--- src/domain/pricing/index.tsx
+++ src/domain/pricing/index.tsx
@@ -1,13 +1,13 @@
 import { Router, RouteComponentProps } from "@reach/router"
-import { navigate } from "gatsby"
 import React, { useState } from "react"
+
 import PlusIcon from "../../components/fundamentals/icons/plus-icon"
 import BodyCard from "../../components/organisms/body-card"
 import TableViewHeader from "../../components/organisms/custom-table-header"
 import PricingTable from "./pricing-table"
 import BulkEditorContainer from "./bulk-editor"
 
-import { MockVariants } from "./bulk-editor/mock.variants"
+import { MockProducts } from "./bulk-editor/mock.products"
 
 const PricingIndex: React.FC<RouteComponentProps> = () => {
   const [show, setShow] = useState(false) // TODO: remove
@@ -30,7 +30,7 @@ const PricingIndex: React.FC<RouteComponentProps> = () => {
           <PricingTable />
         </BodyCard>
       </div>
-      {show && <BulkEditorContainer variants={MockVariants} />}
+      {show && <BulkEditorContainer products={MockProducts} />}
     </div>
   )
 }
