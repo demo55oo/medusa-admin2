2d9b970653bcd57d8302cd8dcfe01a25b008fd42
Ebrahim Sameh <ebrahimsameh16@gmail.com>
2022-05-18 22:12:58 +0200

    Update login-layout.tsx

--- src/components/templates/login-layout.tsx
+++ src/components/templates/login-layout.tsx
@@ -12,10 +12,10 @@ const LoginLayout = ({ children }) => {
         >
           {children}
           <div className="text-grey-0 inter-base-regular pb-12">
-            © Medusa Commerce <span>&#183;</span>{" "}
+            © Carza Commerce 2022<span>&#183;</span>{" "}
             <a
               style={{ color: "inherit", textDecoration: "none" }}
-              href="mailto:hello@medusajs.com"
+              href="mailto:dzulhelmynazri@gmail.com"
             >
               Contact
             </a>
